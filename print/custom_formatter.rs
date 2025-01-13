b42ee743db1bcf59cc0107ad24420de856054d79
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2025-01-13 22:01:22 +0530

    custom format and done with print

---
+++ print/custom_formatter.rs
@@ -0,0 +1,19 @@
+use std::fmt;
+
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} is {} years old", self.name, self.age)
+    }
+}
+
+fn main() {
+    let alice = Person { name: String::from("Alice"), age: 30 };
+    println!("{}", alice);
+}
+
+//Alice is 30 years old
