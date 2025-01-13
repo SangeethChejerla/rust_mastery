ebbfe8b5b5a7bd005845164f05685582a3c110ae
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2025-01-13 21:59:33 +0530

    control precision and binary,octal, hexa format is added

---
+++ print/b_o_x_formatting.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let number = 42;
+    println!("Binary: {:b}, Octal: {:o}, Hexadecimal: {:x}", number, number, number);
+}
---
+++ print/precision_control.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let pi = 3.141592;
+    println!("Pi: {:.2}", pi);
+}
+
+// result : 3.14
